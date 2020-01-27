c91b237b30c305f1242d064e5c24461d9f551ddc
Edson Rodrigo <edson_nt2@hotmail.com>
2020-01-27 13:41:04 -0300

    deploy devconnector2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connectDB = require("../config/db");
 const routes = require("./routes");
-const path = require("path");
+const cors = require("cors");
 const app = express();
 
 // Conectando mongoDB
 connectDB();
+app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(routes);
 
@@ -19,3 +20,10 @@ app.use(routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Servidor conectado na porta ${PORT}`));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../../web/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "..", "..", "web", "build", "index.html"));
+//   });
+// }
